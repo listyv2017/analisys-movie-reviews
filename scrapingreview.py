e8994904943300a3899e1226642c90b66ed1191a
urve <ytcliug284390@gmail.com>
2024-03-05 12:28:18 +0900

    add unit tests fo nw feature

--- scrapingreview.py
+++ scrapingreview.py
@@ -37,6 +37,7 @@ def get_reviews_on_page(soup):
 
 # 単一のページのレビュー取得
 def scrape_single_page(page_url):
+    
     r = session.get(page_url)  # セッションを使用してリクエスト
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, 'html.parser')
@@ -47,7 +48,7 @@ def scrape_single_page(page_url):
 
 # 全ページ分のレビューを取得
 def scrape_reviews(movie_url):
-    
+
     # HTTP GETリクエストを送信してWebページのHTMLデータを取得
     r = session.get(movie_url)
     if r.status_code == 200:
@@ -62,15 +63,26 @@ def scrape_reviews(movie_url):
     last_page = get_last_pagination(soup)
     all_reviews = []
 
-    
+
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        
+
         page_urls = [f"{movie_url}?page={page_number}" for page_number in range(1, last_page + 1)]
         reviews_lists = executor.map(scrape_single_page, page_urls)
+
+        # 処理を開始する前の時間を記録
+        start_time = time.time()
+        
         for reviews in reviews_lists:
             if reviews is not None:  # Noneチェックを追加
                 all_reviews.extend(reviews)
-            
+        
+        # 処理を終了する時間を記録
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("処理にかかった時間:", elapsed_time, "秒")
+             
+
+    
     filename = f"{title}_reviews.jsonl"
     with open(filename, 'w') as f:
         for review in all_reviews:
@@ -78,6 +90,8 @@ def scrape_reviews(movie_url):
             json.dump(data, f, ensure_ascii=False)
             f.write('\n')
     print("作品のレビュー抽出が終了しました。:", title)
+    
+
 
 """  
     # JSONLファイルを読み込んで表示する関数
@@ -91,15 +105,7 @@ def scrape_reviews(movie_url):
 
 if __name__ == "__main__":
     
-    # 処理を開始する前の時間を記録
-    start_time = time.time()
-    
     # WebページのURL
     movie_url = 'https://filmarks.com/movies/109465'
     #reviewを取得
     scrape_reviews(movie_url)
-
-    # 処理を終了する時間を記録
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("処理にかかった時間:", elapsed_time, "秒")
